588b4ce285f2c008a3270934e62686dc4254c7f6
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-02-28 15:57:08 +0000

    introduction updated

--- src/lib/Introduction.svelte
+++ src/lib/Introduction.svelte
@@ -5,16 +5,18 @@
 
 <Page>
     <div class="font-roboto h-[calc(100%_-_150px)] mt-[100px] mb-[50px] mx-[60px] relative">
-        <img src="vector_design_original.svg" alt="cover-design" class="absolute w-[180%] top-[350px] -left-[450px] max-w-none z-10" />
-        <img src="logo_dark.svg" alt="cover-design" class="absolute w-[30%] bottom-0 right-2 z-10" />
+        <img src="vector_design_original.svg" alt="cover-design"
+             class="absolute w-[180%] top-[350px] -left-[450px] max-w-none z-10"/>
+        <img src="logo_dark.svg" alt="cover-design" class="absolute w-[30%] bottom-0 right-2 z-10"/>
         <h1 class="text-3xl text-center uppercase"> Introduction</h1>
         <p class="text-lg mt-6 text-justify indent-[40px]">
-            The students enrolled in the B.Sc. Engineering Honors Degree specializing in Computer Science
-            & Engineering possess the proficiency and aptitude to adapt effectively to digital transformation.
-            As part of their comprehensive academic program, these students are equipped with a broad
-            understanding of computer systems, software development, data analytics, and other relevant fields.
-            The <strong>CSE Careers Day 2023</strong> provides the opportunity for organizations to interact and evaluate the potential
-            of recruiting and employing individuals of such high competence.
+            The students enrolled in the B.Sc. Engineering Honors Degree specializing in Computer Science & Engineering
+            possess the proficiency and aptitude to effectively adapt to digital transformation. As part of their
+            comprehensive academic program, these students have a broad understanding of computer systems, software
+            development, data analytics, and other relevant fields. The <strong>CSE Careers Day 2023</strong> provides
+            the opportunity
+            for organizations to interact and evaluate the potential of recruiting and employing individuals of such
+            high competence.
         </p>
         <p class="text-lg mt-6 text-justify indent-[40px]">
             This booklet consists of the résumés of the CSE undergraduates of the ‘18 Batch who are expected to graduate
@@ -23,8 +25,8 @@
         </p>
         <p class="relative z-50 text-lg mt-6 text-justify indent-[40px]">
             To facilitate convenience of use, these resumes adhere to a standardized format and are organized
-            alphabetically by the first name of each undergraduate. Furthermore, for the purpose of reference by first
-            name located at the end of the booklet, which lists them accordingly.
+            alphabetically by the last name of each undergraduate. Furthermore, an index is located at the end of the
+            booklet for reference by first name, which lists them accordingly.
         </p>
     </div>
 </Page>
